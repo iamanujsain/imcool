1cbac355a61b5d57135b9a20d5d0112f23eb3a3f
Anuj Sain <mailanujsain@yahoo.com>
2018-12-16 00:31:12 +0530

    Created branch shp - serving html pages

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+
+var fs = require('fs');
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello world!</h1>');
+    
+
+    if (req.url === "/index") {
+        fs.readFile("index.html", (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.write("Not Found!");
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            }
+        });
+    } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>This is the default response!</h1>');
+    }
 });
 server.listen(port, () => {
     console.log('Server running at port ' + port);
